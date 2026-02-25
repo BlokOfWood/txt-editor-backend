75f202acbe740359296323da7bed63e689d3380d
Pap Sándor Dániel <pap.sandor.daniel@gmail.com>
2026-02-25 22:23:29 +0100

    convert txt editor to a visual studio project

--- Program.cs
+++ Program.cs
@@ -76,7 +76,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseCors(policy =>
-    policy.WithOrigins("http://localhost:4200")
+    policy.WithOrigins("http://localhost:4200", "https://localhost:4200")
             .AllowCredentials()
           .AllowAnyMethod()
           .AllowAnyHeader());

f2a06dc22e65316ac46c46dee7c6cbdbf978b3e2
Pap Sándor Dániel <pap.sandor.daniel@pm.me>
2026-04-01 02:55:50 +0200

    fixed incorrect message type for document unoccupation message

--- Models/Messages/DocumentUnoccupationMessage.cs
+++ Models/Messages/DocumentUnoccupationMessage.cs
@@ -3,7 +3,7 @@ namespace aresu_txt_editor_backend.Models.Messages;
 
 public class DocumentUnoccupationMessage(int documentId)
 : MessageBase(
-    Enums.OccupancyWebsocketMessageType.REPORT_OCCUPIED_DOCUMENT,
+    Enums.OccupancyWebsocketMessageType.REPORT_FREED_DOCUMENT,
     BitConverter.GetBytes(documentId))
 {
     public int DocumentId = documentId;
